93ed8775b141dc5fc1a7e149ceb61e25ef632732
Nilum <Robert_Schumacher@live.com>
2022-05-09 10:08:07 -0700

    - LinkedList now contains nodes in a simple, straightforward, singular direction of nodes. - Node test success in main.py

--- main.py
+++ main.py
@@ -2,21 +2,14 @@
 # We'll use this space to test nodes and linked lists, so import both.
 # We'll ultimately refactor once the basic concept works.
 from mov_node import Node
-# from mov_linked_list import linked_list
+from mov_linked_list import LinkedList
 
-# Testing node code, to make sure I got it right.
-alien = Node("Alien", 1)
-aliens = Node("Aliens", 2)
-aliens_3 = Node("Alien 3", 3)
-aliens_4 = Node("Alien 4", 4)
+test_list = LinkedList()
+test_list.add_film("Alien", "Sci-Fi")
+test_list.add_film("Alien 2", "Sci-Fi")
+test_list.add_film("Alien 3", "Sci-Fi")
 
-alien.set_next_node(aliens)
-aliens.set_next_node(aliens_3)
-aliens_3.set_next_node(aliens_4)
-
-current_node = alien
+current_node = test_list.head
 while current_node:
     print(current_node.get_film())
     current_node = current_node.get_next_node()
-
-# Success!
--- mov_linked_list.py
+++ mov_linked_list.py
@@ -1,24 +1,31 @@
 # Linked list contains all nodes in order they were inserted.
 # A search function should also be added to allow one to find and collect all instances of an item with a specific tag.
 # eg: Search for all films with the "sci-fi" tag and then return them as a new list.
+from mov_node import Node
+# Keep nodes in their own file to keep things easy reading.
 
-# from mov_node import node <- Keep nodes in their own file to keep things easy reading.
-# Definite improvement from my last project...
 
-# class linked_list
-#   def __init__(self):
-#       self.head = None <- Important! Sets head of list! Do not lose this!! For the love of god!!!
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        # Important! Sets head of list! Do not lose this!! For the love of god!!!
 
-#   def add_film(self, title, tag):
-#       new_node = node(title, tag) <- Establishes a node with film title & film tags.
-#       if self.head is None...
-#           self.head = new_node <- List is empty, so just set new node to be head node.
-#       else...
-#           old_node = self.head_node <- Redundant step that makes me feel safer until I'm ready to refactor.
-#           new_node.set_next_node(old_node) <- Set new node's next node to old node, so we don't lose old node.
-#           self.head = new_node <- Replace old node with new node, now that new_node points to old_node.
+    def add_film(self, title, tag):
+        new_node = Node(title, tag)
+        # Establishes a node with film title & film tags.
+        if self.head is None:
+            self.head = new_node
+            # List is empty, so just set new node to be head node.
+        else:
+            old_node = self.head
+            # Redundant step that makes me feel safer until I'm ready to refactor.
+            new_node.set_next_node(old_node)
+            # Set new node's next node to old node, so we don't lose old node.
+            self.head = new_node
+            # Replace old node with new node, now that new_node points to old_node.
 
-#   def search_tag(self, tag):
+    def search_tag(self, tag):
+        pass
 #       return_list = [] <- This is an empty list that will contain all relevant films.
 #       set current_node to self.head.
 #       if current_node is None...
